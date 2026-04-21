8b2c8e96bbc651646195caf170bc1216c931c1b3
varshasutarprp2-rgb <varsha.sutarprp2@gmail.com>
2026-04-21 17:08:56 +0530

    added so and so

--- Demo.java
+++ Demo.java
@@ -1,6 +1,9 @@
+import java.util.List;
+
 public class Demo {
     public static void main(String[] args) {
 
-
- 
+        var songs = List.of("TUM HI HO", "BEDARDHA");
+        System.out.println(songs);
+    }
 }
